e5dc0909b91ead9455c2b9ece6321d8644d0f46b
Radist <iitonatosii@yandex.ua>
2015-03-07 16:19:45 +0200

    added todo

--- src/main/java/com/sprsec/controller/MarkerController.java
+++ src/main/java/com/sprsec/controller/MarkerController.java
@@ -27,7 +27,7 @@ public class MarkerController {
         markerService.saveMarker(marker);
         return marker;
     }
-            //todo : як вернути string в ajax запросі якщо succsess
+            //todo : як вернути string в ajax запросі якщо succsess =)
 
     @RequestMapping(value = "/remove_marker", method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity<Marker> removeMarker(@RequestBody Marker marker) throws JSONException {
