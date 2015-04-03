c0aa9c6b75d17b69c2a177fb179f79867bdc3e02
Radist <iitonatosii@yandex.ua>
2015-04-03 21:55:30 +0300

    Merge branch 'views' of C:\Users\Radist\IdeaProjects\travelerproject with conflicts.

---
+++ src/main/java/com/sprsec/dao/map/CategoryDaoImpl.java
@@ -0,0 +1,30 @@
+package com.sprsec.dao.map;
+
+import com.sprsec.model.Category;
+import com.sprsec.model.Marker;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.mapping.Collection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Repository
+public class CategoryDAOImpl implements CategoryDAO{
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    private Session getSession() {
+        return sessionFactory.getCurrentSession();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<Category> getCategories() {
+        return getSession().createQuery("from Category").list();
+    }
+}
